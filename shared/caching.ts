5bd934d1122860e39a788cf59e87d04362032211
Adam-Golan <slowmoon.adam@gmail.com>
2024-12-08 19:03:01 +0200

    Deactivating auto chaching activation.

--- shared/caching.ts
+++ shared/caching.ts
@@ -1,27 +1,6 @@
-/**
- * Enhances an object by adding caching functionality to its methods.
- * 
- * This function wraps the methods of the given object in a Proxy, caching
- * their results based on the arguments passed. If a method is called again
- * with the same arguments, the cached result is returned instead of calling
- * the method again.
- * 
- * @param obj - The object whose methods will be enhanced with caching.
- */
 function addCaching<T extends object>(obj: T): void {
     const cache = new Map<string, any>();
     const cachedMethods = new Proxy(obj, {
-      /**
-       * Caches the result of a method call with the given arguments. If the method
-       * is called again with the same arguments, the cached result is returned
-       * instead of calling the method again.
-       *
-       * @param target The object that contains the method.
-       * @param prop The name of the method to cache.
-       * @returns A function that calls the original method, caches the result, and
-       * returns the cached result if the method is called again with the same
-       * arguments.
-       */
       get(target, prop) {
         if (typeof target[prop] === 'function') {
           return function (...args: any[]) {
@@ -42,15 +21,15 @@ function addCaching<T extends object>(obj: T): void {
     Object.setPrototypeOf(obj, cachedMethods);
   }
 
-  addCaching(String.prototype);
-  addCaching(Number.prototype);
-  addCaching(BigInt.prototype);
-  addCaching(Array.prototype);
-  addCaching(Object.prototype);
-  addCaching(Function.prototype);
-  addCaching(Promise.prototype);
-  addCaching(JSON);
-  addCaching(Date.prototype);
-  addCaching(Set.prototype);
-  addCaching(Map.prototype);
-  addCaching(RegExp.prototype);
+  // addCaching(String.prototype);
+  // addCaching(Number.prototype);
+  // addCaching(BigInt.prototype);
+  // addCaching(Array.prototype);
+  // addCaching(Object.prototype);
+  // addCaching(Function.prototype);
+  // addCaching(Promise.prototype);
+  // addCaching(JSON);
+  // addCaching(Date.prototype);
+  // addCaching(Set.prototype);
+  // addCaching(Map.prototype);
+  // addCaching(RegExp.prototype);
